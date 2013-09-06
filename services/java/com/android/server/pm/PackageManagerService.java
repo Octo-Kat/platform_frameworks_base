34ea9af4208637d8776f03a0c2a966d23020d2b9
Danesh M <daneshm90@gmail.com>
2013-09-05 19:23:15 -0700

    Privacy Guard : Reimplement backed by AppOps[1/3]
    Re-implements privacy guard to use appops.
    
    Change-Id: I2b52e8cf0d6e126fb156f83df885b5551503cd9d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -52,6 +52,7 @@ import org.xmlpull.v1.XmlSerializer;
 
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
+import android.app.AppOpsManager;
 import android.app.IActivityManager;
 import android.app.IconPackHelper;
 import android.app.admin.IDevicePolicyManager;
@@ -499,6 +500,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     ComponentName mCustomResolverComponentName;
 
     boolean mResolverReplaced = false;
+    private AppOpsManager mAppOps;
 
     private IconPackHelper mIconPackHelper;
 
@@ -951,6 +953,18 @@ public class PackageManagerService extends IPackageManager.Stub {
                                 deleteOld = true;
                             }
 
+                            if (!update && !isSystemApp(res.pkg.applicationInfo)) {
+                                boolean privacyGuard = Secure.getIntForUser(
+                                        mContext.getContentResolver(),
+                                        android.provider.Settings.Secure.PRIVACY_GUARD_DEFAULT,
+                                        0, UserHandle.USER_CURRENT) == 1;
+                                if (privacyGuard) {
+                                    mAppOps.setPrivacyGuardSettingForPackage(
+                                            res.pkg.applicationInfo.uid,
+                                            res.pkg.applicationInfo.packageName, true);
+                                }
+                            }
+
                             // Log current value of "unknown sources" setting
                             EventLog.writeEvent(EventLogTags.UNKNOWN_SOURCES_ENABLED,
                                 getUnknownSourcesSettings());
@@ -1170,6 +1184,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         mSettings.addSharedUserLPw("android.uid.shell", SHELL_UID,
                 ApplicationInfo.FLAG_SYSTEM|ApplicationInfo.FLAG_PRIVILEGED);
 
+        mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
         String separateProcesses = SystemProperties.get("debug.separate_processes");
         if (separateProcesses != null && separateProcesses.length() > 0) {
             if ("*".equals(separateProcesses)) {
