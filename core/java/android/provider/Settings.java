bc036773c52677ce9436dba6a52cff22665109ea
MassStash <MassStash@gmail.com>
2014-02-24 10:56:21 -0600

    Statusbar clock font style (1/2) PS1: Initial PS2: Int settings
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950
    
    Clock Font: Adding more styles
    Change-Id: I7ff863fba0c33409f6cad28df5bf3003db2aae0f
    Thanks CarbonDev
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3262,6 +3262,12 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
+        
+        /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
 
         /**
         * @hide
