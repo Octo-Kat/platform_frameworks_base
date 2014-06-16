5ee2fbcf9c7e8e4d36462ee93dcdc39bfabf54c8
treken <chupekevin@gmail.com>
2014-06-16 15:58:36 -0400

    Revert "PEEK [1/2]"
    This reverts commit a1cdacf9cd1705ea83f700150e255e91068f5382.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3793,14 +3793,6 @@ public final class Settings {
         public static final String QUIET_HOURS_RINGER = "quiet_hours_ringer";
 
         /**
-         * Either if notification peek is enabled of disabled
-         * 0 = disabled (default)
-         * 1 = enabled
-         * @hide
-         */
-        public static final String PEEK_STATE = "peek_state";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
